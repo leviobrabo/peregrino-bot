0aada6daffa4de0016849aa6c1ce9483b886bcc3
leviobrabo <carlosjunior20313@gmail.com>
2023-12-17 15:33:11 -0300

    Alteração na formação básica do logger

--- peregrino/loggers.py
+++ peregrino/loggers.py
@@ -4,9 +4,9 @@ from .config import LOG_PATH
 
 logger.add(
     LOG_PATH,
-    level="DEBUG",
-    format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {message}",
-    rotation="500 MB",
+    level='DEBUG',
+    format='{time:YYYY-MM-DD HH:mm:ss} | {level} | {message}',
+    rotation='500 MB',
     serialize=True,
     backtrace=True,
     diagnose=True,
