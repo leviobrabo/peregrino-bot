43b105125b043bc1af349229c365e433f2951173
leviobrabo <carlosjunior20313@gmail.com>
2023-12-17 15:33:35 -0300

    Alteração na formação da versão do bot

--- peregrino/version.py
+++ peregrino/version.py
@@ -4,4 +4,4 @@ import telebot
 
 python_version = f'{version_info[0]}.{version_info[1]}.{version_info[2]}'
 peregrino_version = '0.1.0'
-telebot_version = getattr(telebot, '__version__', "Versão não encontrada")
+telebot_version = getattr(telebot, '__version__', 'Versão não encontrada')
