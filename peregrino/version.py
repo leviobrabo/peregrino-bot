19267fdebbf492d2a47555bff760ba0894a1027e
leviobrabo <carlosjunior20313@gmail.com>
2023-12-17 15:03:22 -0300

    Adicionado o version do projeto

---
+++ peregrino/version.py
@@ -0,0 +1,7 @@
+from sys import version_info
+
+import telebot
+
+python_version = f'{version_info[0]}.{version_info[1]}.{version_info[2]}'
+peregrino_version = '0.1.0'
+telebot_version = getattr(telebot, '__version__', "Versão não encontrada")
