45fa159b407b814af994b24fbba65e13b26e84c1
leviobrabo <carlosjunior20313@gmail.com>
2023-12-17 15:03:46 -0300

    Adicionado o log do bot

---
+++ peregrino/loggers.py
@@ -0,0 +1,14 @@
+from loguru import logger
+
+from .config import LOG_PATH
+
+logger.add(
+    LOG_PATH,
+    level="DEBUG",
+    format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {message}",
+    rotation="500 MB",
+    serialize=True,
+    backtrace=True,
+    diagnose=True,
+    enqueue=True,
+)
